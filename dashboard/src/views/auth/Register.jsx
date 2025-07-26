1f6c0724575e94bac1ddda59459a8615a1b02d25
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-26 13:17:30 +0200

    Route and Login Register Part 3

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
  const Register = () => {
   return (
@@ -16,6 +17,37 @@ import React from 'react'
                            type="text" name="name" id="name" 
                            placeholder='Name' required />
                    </div>
+                   <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="email">Email</label>
+                        <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' 
+                           type="email" name="email" id="email" 
+                           placeholder='Email' required />
+                   </div>
+                   <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="password">Password</label>
+                        <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' 
+                           type="password" name="password" id="password" 
+                           placeholder='Password' required />
+                   </div>
+                   <div className='flex items-center w-full gap-3 mb-3'>
+                       <input className='w-4 h-4 text-blue-600
+                        focus:ring-blue-500 bg-gray-200 rounded border-gray-300
+                         overflow-hidden'
+                          type="checkbox" name="checkbox" id="checkbox" />
+                          <label htmlFor="checkbox">I agree to privacy policy & treams</label>
+                   </div>
+                   <button className='bg-slate-800 w-full
+                    hover:shadow-blue-300/50 hover:shadow-lg
+                     text-white rounded-md px-7 py-2 mb-3'>
+                      Sign Up
+                    </button>
+                    <div className='flex items-center mb-3 gap-3 justify-center'>
+                      <p>
+                        Already Have an account ?
+                         <Link to='/login' className='font-bold'> Sign In</Link>
+                      </p>
+
+                    </div>
                 </form>
             </div>
         </div>
