d351f77ed64a857f969bbf6325ccb360d699eba6
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-28 10:05:57 +0200

    Project Backend Setup Part 2

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 const app = express()
 require('dotenv').config() 
 
+// appel les routes
+const authRoutes = require('./routes/authRoutes')
+
+app.use('/api', authRoutes)
+
 app.get('/', (req, res) => res.send("My backend"))
 
 const port = process.env.PORT
