f93fb4da132470be907b36a10b200affe55c97ec
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-30 01:49:42 +0200

    Project Navigation Part 4

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { BiLogOutCircle } from "react-icons/bi";
 import { getNav } from '../navigation/index'
 
 const Sidebar = () => {
@@ -32,6 +33,14 @@ const Sidebar = () => {
                              </Link>
                         </li>)
                     }
+
+                    <li>
+                        <button className='text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
+                          <span><BiLogOutCircle /></span>
+                          <span>Logout</span>
+                       </button>
+                    </li>
+
                  </ul>
               </div>
         </div>
