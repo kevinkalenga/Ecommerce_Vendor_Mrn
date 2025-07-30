b27690dcfbb086329fe07e581435f20adeb9040c
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-30 03:15:07 +0200

    Project Header Design Part 2

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom'
 import { BiLogOutCircle } from "react-icons/bi";
 import { getNav } from '../navigation/index'
 
-const Sidebar = () => {
+const Sidebar = ({showSidebar, setShowSidebar}) => {
     const [allNav, setAllNav] = useState([])
     const {pathname} = useLocation();
     useEffect(() => {
@@ -13,9 +13,14 @@ const Sidebar = () => {
     // console.log(allNav)
   return (
     <div>
-        <div></div>
+     
+        <div onClick={() => setShowSidebar(false)} 
+          className={`fixed duration-200 ${!showSidebar ? 'invisible':'visible'}
+           w-screen h-screen bg-[#8cbce780] top-0 left-0 z-10`}>
+
+        </div>
         <div className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 
-            h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}>
+            h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? 'left-0':'-left-[260px] lg:left-0'}`}>
               <div className='h-[70px] flex justify-center items-center'>
                  <Link to='/' className='w-[180px] h-[50px]'>
                     <img className='w-full h-full' src="http://localhost:3000/images/logo.png" alt="" />
