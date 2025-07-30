edfcdcf2b76897527c33c9f0853038dc10d40335
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-30 03:33:45 +0200

    Project Header Design Part 3

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -16,6 +16,22 @@ const Header = ({showSidebar, setShowSidebar}) => {
                     <FaBars />
                   </span>
             </div>
+            <div className='hidden md:block'>
+                <input className='px-3 py-2 outline-none border bg-transparent
+                   border-slate-700 rounded-md text-[#423d72]
+                    focus:border-indigo-300 overflow-hidden' type="text" name='search' placeholder='Search' />
+            </div>
+            <div className='flex justify-center items-center gap-8 relative'>
+                <div className='flex justify-center items-center'>
+                   <div className='flex justify-center items-center gap-3'>
+                      <div className='flex justify-center items-center flex-col text-end'>
+                           <h2 className='text-md font-bold'>Kevin Kalenga</h2>
+                           <span className='text-[14px] w-full font-normal'>Admin</span>
+                      </div>
+                      <img src="http://localhost:3000/images/admin.jpg" alt="" className='w-[45px] h-[45px] rounded-full overflow-hidden' />
+                   </div>
+                </div>
+            </div>
         </div>
     </div>
   )
