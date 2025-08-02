53fd2329b53ec4d94b070886613b516b0e21a026
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-02 18:48:38 +0200

    Admin Order Page Design Part 1

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,7 +1,9 @@
 
 import { lazy } from "react";
 
+
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
+const Orders = lazy(() => import('../../views/admin/Orders'));
 
 
 
@@ -10,5 +12,10 @@ export const adminRoutes = [
         path: 'admin/dashboard',
         element : <AdminDashboard />,
         role : 'admin'
-    }
+    },
+    {
+        path: 'admin/dashboard/orders',
+        element : <Orders />,
+        role : 'admin'
+    },
 ]
