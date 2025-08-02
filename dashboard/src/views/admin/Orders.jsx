dc50c633b04b914c2d749fcd9429a00e4330e939
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-02 19:24:12 +0200

    Admin Order Page Design Part 2

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-
+import { BsArrowDownSquare } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 const Orders = () => {
   
     const [currentPage, setCurrentPage] = useState(1)
@@ -21,6 +22,39 @@ const Orders = () => {
                 <input className='px-4 py-2 focus:border-indigo-500
                   outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text[#d0d2d6]' 
                    type="text" placeholder='Search'/>
+            </div>
+            <div className='relative mt-5 overflow-x-auto'>
+                 <div className='w-full text-sm text-left [#d0d2d6]'>
+                     <div className='text-sm text-[#d0d2d6]
+                       uppercase border-b border-slate-700'>
+                         <div className='flex justify-between items-center'>
+                              <div className='py-3 w-[25%] font-bold'>Order id</div>
+                              <div className='py-3 w-[13%] font-bold'>Price</div>
+                              <div className='py-3 w-[18%] font-bold'>Payment Status</div>
+                              <div className='py-3 w-[18%] font-bold'>Order Status</div>
+                              <div className='py-3 w-[18%] font-bold'>Action</div>
+                              <div className='py-3 w-[8%] font-bold'>
+                                 <BsArrowDownSquare />
+                              </div>
+                         </div>
+
+                     </div>
+                     <div className=' text-[#d0d2d6]'>
+                         <div className='flex justify-between items-start border-b border-slate-700'>
+                              <div className='py-3 w-[25%] font-medium whitespace-nowrap'>#34343</div>
+                              <div className='py-3 w-[13%] font-medium'>$654</div>
+                              <div className='py-3 w-[18%] font-medium'>Pending</div>
+                              <div className='py-3 w-[18%] font-medium'>Pending</div>
+                              <div className='py-3 w-[18%] font-medium'>
+                                <Link>View</Link>
+                              </div>
+                              <div className='py-3 w-[8%] font-medium'>
+                                 <BsArrowDownSquare />
+                              </div>
+                         </div>
+
+                     </div>
+                 </div>
             </div>
         </div>
     </div>
