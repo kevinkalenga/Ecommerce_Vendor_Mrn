bae0c093178e4b43a80e70daf4205e9a7f01fd0b
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-14 02:19:44 +0200

    Payment Request Page Design Part 1

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,12 +1,11 @@
 
 import { lazy } from "react";
 
-
-
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
 const Orders = lazy(() => import('../../views/admin/Orders'));
 const Category = lazy(() => import("../../views/admin/Category"));
 const Sellers = lazy(() => import("../../views/admin/Sellers"));
+const PaymentRequest = lazy(() => import("../../views/admin/PaymentRequest"));
 
 
 
@@ -29,6 +28,11 @@ export const adminRoutes = [
     {
         path: 'admin/dashboard/sellers',
         element : <Sellers />,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/payment-request',
+        element : <PaymentRequest />,
         role : 'admin'
     },
 ]
